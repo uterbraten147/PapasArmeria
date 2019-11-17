a3f37b8e04e99ff2d1b31bd0bca0cc7f2a392061
uterbraten147 <147and@gmail.com>
2019-11-16 19:35:55 -0600

    menu V2

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -8,24 +8,30 @@ using DG.Tweening;
 
 public class MenuController : MonoBehaviour
 {
-	public RectTransform mainMenu, CreditosMenu;
+	public RectTransform mainMenu, CreditosMenu, imagenMenu, BotonesMenu;
     // Start is called before the first frame update
     void Start()
     {
 	    mainMenu.DOAnchorPos(Vector2.zero, 0.25f);
+	    imagenMenu.DOAnchorPos(Vector2.zero, 0.40f).SetDelay(0.25f);
+	    BotonesMenu.DOAnchorPos(Vector2.zero, 0.40f).SetDelay(0.25f);
     }
 
    
 	public void CreditsBTN()
 	{
-		mainMenu.DOAnchorPos(new Vector2(-2000, 0), 0.25f);
+		mainMenu.DOAnchorPos(new Vector2(0, 2000), 0.25f);
+		imagenMenu.DOAnchorPos(new Vector2(2000, 0), 0.35f);
+		BotonesMenu.DOAnchorPos(new Vector2(-2000, 0), 0.35f);
 		CreditosMenu.DOAnchorPos(new Vector2(0,0), 0.25f).SetDelay(0.25f);
 	}
 	
 	public void BackMenu()
 	{
-		CreditosMenu.DOAnchorPos(new Vector2(2000, 0), 0.25f);
+		CreditosMenu.DOAnchorPos(new Vector2(0, -2000), 0.25f);
 		mainMenu.DOAnchorPos(new Vector2(0,0), 0.25f).SetDelay(0.25f);
+		imagenMenu.DOAnchorPos(new Vector2(0,0), 0.25f).SetDelay(0.25f);
+		BotonesMenu.DOAnchorPos(new Vector2(0,0), 0.25f).SetDelay(0.25f);
 	}
 	
 	public void startGame()
