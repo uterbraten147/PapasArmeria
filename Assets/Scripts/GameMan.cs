c084fa49ae08555d5cf9603a8aa5118c92267ce6
Felipe <felipeortega94@gmail.com>
2019-11-25 16:43:21 -0600

    Camaras

---
+++ Assets/Scripts/GameMan.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+public class GameMan : MonoBehaviour
+{
+	
+	public GameObject  PanelOpciones, MesaPanel;
+	public CinemachineVirtualCamera VCamMesa, VCamOptions;
+    // Start is called before the first frame update
+    void Start()
+	{
+		PanelOpciones.SetActive(false);
+		MesaPanel.SetActive(true);
+		VCamMesa.Priority = 10;
+		VCamOptions.Priority= 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+	public void SettingsBTN()
+	{
+		PanelOpciones.SetActive(true);
+		MesaPanel.SetActive(false);
+		VCamMesa.Priority = 0;
+		VCamOptions.Priority= 10;
+		
+	}
+	
+	public void MesaBTN()
+	{
+		PanelOpciones.SetActive(false);
+		MesaPanel.SetActive(true);
+		VCamMesa.Priority = 10;
+		VCamOptions.Priority= 0;
+		
+	}
+}
